da13b88046396157d8f6baadc41b4191a2f33caa
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-27 18:49:10 +0200

    Add target='_blank' to the links in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p className='footer__paragraph'>
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
-                Coded by <a href="https://github.com/pawelpiotrowski38" >Paweł Piotrowski</a>.
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target='_blank' rel="noreferrer">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/pawelpiotrowski38" target='_blank' rel="noreferrer">Paweł Piotrowski</a>.
             </p>
         </footer>
     )
